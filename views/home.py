5315c5e4bf0564241135688166ffdba2d63c5974
serwinze <winzeser@students.zhaw.ch>
2026-03-05 13:53:19 +0100

    Beschreibung hinzugefügt

--- views/home.py
+++ views/home.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
-st.title("Meine erste Streamlit App")
+st.title("Verdünnungsrechner")
+
+st.write("Dieser Rechner hilft dabei, Verdünnungen nach der Formel C1 × V1 = C2 × V2 zu berechnen. "
+         "Gib einfach deine Konzentrationen und Volumina ein und der Rechner bestimmt das fehlende Volumen.")
 
 # !! WICHTIG: Eure Emails müssen in der App erscheinen!!
 
